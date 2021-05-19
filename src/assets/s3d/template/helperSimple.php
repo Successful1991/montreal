bb99d54ce1567b217e63a5e3569a34e8c9ff28bd
Artem Tsys <19successful91@gmail.com>
2021-05-19 10:10:30 +0300

    update helper

---
+++ src/assets/s3d/template/helperSimple.php
@@ -0,0 +1,10 @@
+<?php
+echo json_encode('<div class="s3d__helper__wrap js-s3d__helper-wrap">
+    <div class="s3d__helper__active js-s3d__helper__active"></div>
+        <div class="s3d__helper s3d__helper__empty js-s3d__helper" data-step="0">
+            <div class="s3d__helper__close js-s3d__helper__close" data-type="close"></div>
+            <img src="/wp-content/themes/montreal/assets/s3d/images/helper-rotate.gif" alt="">
+            <div class="s3d__helper__title"  data-type="title"></div>
+        </div>
+    </div>')
+?>
